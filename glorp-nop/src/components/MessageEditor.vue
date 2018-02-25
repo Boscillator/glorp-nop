4cdda70fc699efc0e1cc8100785cca6bcbe656fb
Fred Buchanan <fred@pineapplecomputerart.com>
2018-02-24 20:11:55 -0500

    Friends now autocomplete

--- glorp-nop/src/components/MessageEditor.vue
+++ glorp-nop/src/components/MessageEditor.vue
@@ -1,12 +1,11 @@
 <template>
-  <md-dialog :md-active="shown">
+  <md-dialog id="editor" :md-active="shown">
       <md-dialog-title>Compose Message</md-dialog-title>
       <template v-if="!isSending">
         <md-dialog-content>
-            <md-field>
+            <md-autocomplete v-model="to" :md-options="friends">
                 <label>To</label>
-                <md-input v-model="to"></md-input>
-            </md-field>
+            </md-autocomplete>
             <md-field>
               <label>Message</label>
               <md-input v-model="body"></md-input>
@@ -29,39 +28,50 @@
 
 <script>
 export default {
-    name: "MessageEditor",
-    data: () => ({
-        to: "",
-        body: ""
-    }),
-    computed: {
-        shown() {
-            return this.$store.state.show_message_editor
-        },
-        isSending() {
-            return this.$store.state.sending_message
-        }
+  name: "MessageEditor",
+  data: () => ({
+    to: "",
+    body: ""
+  }),
+  computed: {
+    shown() {
+      return this.$store.state.show_message_editor;
     },
-    methods: {
-        clear() {
-            this.to = "",
-            this.body = ""
-        },
-        cancel() {
-            this.$store.commit('hideMessageEditor')
-            this.clear()
-        },
-        send() {
-            this.$store.dispatch('sendMessage', {
-                to: this.to,
-                body: this.body
-            })
-            this.clear()
-        }
+    isSending() {
+      return this.$store.state.sending_message;
+    },
+    friends() {
+      let f = this.$store.state.friends.map(friend => friend.email)
+      console.log(f);
+      return f;
     }
-}
+  },
+  methods: {
+    clear() {
+      (this.to = ""), (this.body = "");
+    },
+    cancel() {
+      this.$store.commit("hideMessageEditor");
+      this.clear();
+    },
+    send() {
+      this.$store.dispatch("sendMessage", {
+        to: this.to,
+        body: this.body
+      });
+      this.clear();
+    }
+  }
+};
 </script>
 
 <style>
+#editor {
+    width: 120%;
+    z-index: 10;
+}
+.md-menu-content {
+    z-index: 110;
+  }
 
 </style>
